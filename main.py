98c2f1dd182f0d4c75448eb8fe34a308dc4f7ab8
Sorrlag <sorrlag@gmail.com>
2024-03-29 16:39:32 +0300

    prebeta 0.2

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from tkinter import *
 from tkinter import ttk
 import tkcalendar
 import re
-import warnings
 from datetime import datetime
 import modbus_tk.defines as comfunc
 import modbus_tk.modbus_tcp as modbus_tcp
@@ -21,8 +20,8 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import pandas
 
-xLpos, xVpos, yPos = 20, 220, 100
-localIP, panelIP, panelDate, panelTime, currentTime, filename = "", "", "", "", "", ""
+xLabelPos, xValuePos, yPos = 20, 220, 100
+localIP, panelIP, panelDate, panelTime, currentTime, filename, picname = "", "", "", "", "", "", ""
 fg, fgv, bg = "white", "yellow", "black"
 status = ("НЕТ СВЯЗИ", "АВАРИЯ", "РАБОТА", "ОСТАНОВ")
 mode = ("НЕТ СВЯЗИ", "НАСТРОЙКА", "ТЕРМО", "ВЛАГА")
@@ -30,31 +29,32 @@ statusIndex, modeIndex = 0, 0
 temperatureCurrent, temperatureSet = -1.1, -1.1
 humidityCurrent, humiditySet = 1.1, 1.1
 rootFolder = f"C:\\Cont\\"
+picFolder = f"{rootFolder}Graph\\"
 converter = f"{rootFolder}Converter\\easyсonverter.exe"
 dtlFolder = f"{rootFolder}Converter\\data\\"
-csvFolder = f"{rootFolder}_Data\\CSV\\"
-xlsFolder = f"{rootFolder}_Data\\XLS\\"
+csvFolder = f"{rootFolder}\\CSV\\"
+xlsFolder = f"{rootFolder}\\XLS\\"
 mainIP = ""
 
 
 def ObjectsPlace():
     logoLabel.place(x=450, y=780)
-    armIPLabel.place(x=xLpos, y=15)
-    panelIPLabel.place(x=xLpos, y=35)
+    armIPLabel.place(x=xLabelPos, y=15)
+    panelIPLabel.place(x=xLabelPos, y=35)
     panelDateLabel.place(x=650, y=15, width=100)
     panelTimeLabel.place(x=650, y=35, width=100)
-    statusLabel.place(x=xLpos, y=yPos, width=200)
-    statusValue.place(x=xVpos, y=yPos, width=150)
-    modeLabel.place(x=xLpos, y=yPos + 20, width=200)
-    modeValue.place(x=xVpos, y=yPos + 20, width=150)
-    tempCurLabel.place(x=xLpos, y=yPos + 40, width=200)
-    tempCurValue.place(x=xVpos, y=yPos + 40, width=150)
-    tempSetLabel.place(x=xLpos, y=yPos + 60, width=200)
-    tempSetValue.place(x=xVpos, y=yPos + 60, width=150)
-    humCurLabel.place(x=xLpos, y=yPos + 80, width=200)
-    humCurValue.place(x=xVpos, y=yPos + 80, width=150)
-    humSetLabel.place(x=xLpos, y=yPos + 100, width=200)
-    humSetValue.place(x=xVpos, y=yPos + 100, width=150)
+    statusLabel.place(x=xLabelPos, y=yPos, width=200)
+    statusValue.place(x=xValuePos, y=yPos, width=150)
+    modeLabel.place(x=xLabelPos, y=yPos + 20, width=200)
+    modeValue.place(x=xValuePos, y=yPos + 20, width=150)
+    tempCurLabel.place(x=xLabelPos, y=yPos + 40, width=200)
+    tempCurValue.place(x=xValuePos, y=yPos + 40, width=150)
+    tempSetLabel.place(x=xLabelPos, y=yPos + 60, width=200)
+    tempSetValue.place(x=xValuePos, y=yPos + 60, width=150)
+    humCurLabel.place(x=xLabelPos, y=yPos + 80, width=200)
+    humCurValue.place(x=xValuePos, y=yPos + 80, width=150)
+    humSetLabel.place(x=xLabelPos, y=yPos + 100, width=200)
+    humSetValue.place(x=xValuePos, y=yPos + 100, width=150)
     labelPeriods.place(x=410, y=90)
 
 
@@ -77,16 +77,19 @@ def LabelsShow():
     humSetLabel["text"] = f"Уставка по влажности:"
     humSetValue["text"] = f"{humiditySet} °C"
     labelPeriods["text"] = "Диапазон отображения:"
+    buttonSave["text"] = f"Сохранить график в {picFolder}"
+
     root.after(1000, LabelsShow)
 
 
-def ComboboxShow():
+def GraphControl():
     global graphLabels, graphPeriods, graphDefault
     graphLabels = ["1 минута", "5 минут", "15 минут", "30 минут", "1 час", "2 часа", "Настраиваемый..."]
-    graphPeriods = ["00:01:00", "00:05:00", "00:15:00", "00:30:00", "01:00:00", "02:00:00", "00:01:00"]
+    graphPeriods = ["00:01:00", "00:05:00", "00:15:00", "00:30:00", "01:00:00", "02:00:00", "24:01:00"]
     graphDefault = StringVar(value=graphLabels[0])
     graphPeriod = ttk.Combobox(values=graphLabels, textvariable=graphDefault, width=20, state="readonly")
     graphPeriod.place(x=570, y=90)
+    buttonSave.place(x=410, y=210, width=280)
 
 
 def GetLocalIP():
@@ -147,12 +150,11 @@ def CheckIP():
 
 
 def OpenConnection():
-    global mainIP, master, ftp, fileList
+    global mainIP, master, ftp, fileList, csvFolder, xlsFolder
     try:
         master = modbus_tcp.TcpMaster(host=mainIP, port=502, timeout_in_sec=5)
         master.set_timeout(5.0)
     except TimeoutError:
-        print("Error open TCP port")
         ConnectionError()
         return
     try:
@@ -160,17 +162,19 @@ def OpenConnection():
         ftp.login(user="uploadhis", passwd="111111")
         ftp.cwd("datalog/data")
         fileList = ftp.nlst()
+        csvFolder = f"{rootFolder}{mainIP}\\CSV\\"
+        xlsFolder = f"{rootFolder}{mainIP}\\XLS\\"
     except TimeoutError:
-        print("FTP connection failure")
         ConnectionError()
         return
     except AttributeError:
-        print("Wrong host")
         ConnectionError()
         return
+    except ftplib.error_perm:
+        print("Error open FTP")
     ReadModbusTCP()
     LabelsShow()
-    ComboboxShow()
+    GraphControl()
     FTPhistory()
     CSVhistory()
     CurrentUpdate()
@@ -183,17 +187,14 @@ def ConnectionError():
         if wait > 1:
             wait -= 1
             info["text"] = f"Ошибка соединения...\nПовтор через: {wait}"
-            print(wait)
             screenError.after(1000, Countdown)
         else:
             RetryConnection()
     def RetryConnection():
-        print("Retry connection...")
         screenError.grab_release()
         screenError.destroy()
         OpenConnection()
     def ResetIP():
-        print("IP is changing...")
         screenError.grab_release()
         screenError.destroy()
         InputIP()
@@ -214,9 +215,9 @@ def ConnectionError():
 
     info = tkinter.Label(master=screenError, bg="yellow")
     info.place(x=10, y=10, width=280)
-    buttonRetry = tkinter.Button(master=screenError, text="Повторить", command=RetryConnection)
-    buttonChange = tkinter.Button(master=screenError, text="Изменить IP", command=ResetIP)
-    buttonClose = tkinter.Button(master=screenError, text="Завершить", command=sys.exit)
+    buttonRetry = ttk.Button(master=screenError, text="Повторить", command=RetryConnection)
+    buttonChange = ttk.Button(master=screenError, text="Изменить IP", command=ResetIP)
+    buttonClose = ttk.Button(master=screenError, text="Завершить", command=sys.exit)
     buttonRetry.place(x=15, y=60, width=80)
     buttonChange.place(x=110, y=60, width=80)
     buttonClose.place(x=205, y=60, width=80)
@@ -224,7 +225,7 @@ def ConnectionError():
 
 
 def ReadModbusTCP():
-    global panelIP, panelDate, panelTime, currentTime, filename, \
+    global panelIP, panelDate, panelTime, currentTime, filename, picname, \
         temperatureCurrent, temperatureSet, humidityCurrent, humiditySet, modeIndex, statusIndex
     try:
         getSys = master.execute(1, comfunc.READ_INPUT_REGISTERS, 10099, 10)
@@ -234,31 +235,42 @@ def ReadModbusTCP():
         getHumSet = master.execute(1, comfunc.READ_INPUT_REGISTERS, 10112, 1)
         getStatus = master.execute(1, comfunc.READ_INPUT_REGISTERS, 10115, 1)
         getMode = master.execute(1, comfunc.READ_INPUT_REGISTERS, 10114, 1)
+        panelIP = f"{getSys[0]}.{getSys[1]}.{getSys[2]}.{getSys[3]}"
+        panelDate = f"{getSys[4]:02} / {getSys[5]:02} / {getSys[6]}"
+        panelTime = f"{getSys[7]:02} : {getSys[8]:02} : {getSys[9]:02}"
+        currentTime = f"{getSys[7]:02}:{getSys[8]:02}:{getSys[9]:02}"
+        filename = f"{getSys[6]:04}{getSys[5]:02}{getSys[4]:02}"
+        picname = f"{getSys[6]}{getSys[5]:02}{getSys[4]:02}_{getSys[7]:02}{getSys[8]:02}{getSys[9]:02}"
+        temperatureCurrent = (getTempCur[0] - 2 ** 16) / 10 if getTempCur[0] > 2 ** 15 else getTempCur[0] / 10
+        temperatureSet = (getTempSet[0] - 2 ** 16) / 10 if getTempSet[0] > 2 ** 15 else getTempSet[0] / 10
+        humidityCurrent = getHumCur[0] / 10
+        humiditySet = getHumSet[0]
+        statusIndex = int(getStatus[0])
+        modeIndex = int(getMode[0])
+    except UnboundLocalError:
+        print("Modbus format error")
     except TimeoutError:
-        print("TCP connection failure")
         ConnectionError()
         return
-    panelIP = f"{getSys[0]}.{getSys[1]}.{getSys[2]}.{getSys[3]}"
-    panelDate = f"{getSys[4]:02} / {getSys[5]:02} / {getSys[6]}"
-    panelTime = f"{getSys[7]:02} : {getSys[8]:02} : {getSys[9]:02}"
-    currentTime = f"{getSys[7]:02}:{getSys[8]:02}:{getSys[9]:02}"
-    filename = f"{getSys[6]:04}{getSys[5]:02}{getSys[4]:02}"
-    temperatureCurrent = (getTempCur[0] - 2 ** 16) / 10 if getTempCur[0] > 2 ** 15 else getTempCur[0] / 10
-    temperatureSet = (getTempSet[0] - 2 ** 16) / 10 if getTempSet[0] > 2 ** 15 else getTempSet[0] / 10
-    humidityCurrent = getHumCur[0] / 10
-    humiditySet = getHumSet[0]
-    statusIndex = int(getStatus[0])
-    modeIndex = int(getMode[0])
-
+    except ConnectionRefusedError:
+        print("Modbus read error")
     root.after(1000, ReadModbusTCP)
 
 
 def FTPhistory():
-    for fileNum in fileList:
-        remoteFile = fileNum
-        localFile = f"{dtlFolder}{remoteFile}"
-        with open(localFile, "wb") as file:
-            ftp.retrbinary("RETR %s" % remoteFile, file.write)
+    try:
+        for fileNum in fileList:
+            remoteFile = fileNum
+            localFile = f"{dtlFolder}{remoteFile}"
+            with open(localFile, "wb") as file:
+                ftp.retrbinary("RETR %s" % remoteFile, file.write)
+    except NameError:
+        print("Error read file list")
+        time.sleep(1)
+        FTPhistory()
+    except TimeoutError:
+        ConnectionError()
+        return
 
 
 def CSVhistory():
@@ -278,7 +290,10 @@ def CurrentUpdate():
         with open(localFile, "wb") as file:
             ftp.retrbinary("RETR %s" % remoteFile, file.write)
     except TimeoutError:
-        print("FTP failure")
+        ConnectionError()
+        return
+    except NameError:
+        print("Error open FTP document")
         return
     dtlList = os.listdir(dtlFolder)
     dtlFile = ''.join(dtlList[-1:])
@@ -304,43 +319,70 @@ def WriteFile():
 
 def Plot():
     fileList = os.listdir(csvFolder)
-    filePath = csvFolder + ''.join(fileList[-1:])
+    filePath = csvFolder + ''.join(fileList[-1])
     choice = graphDefault.get()
     choiceTime = graphPeriods[graphLabels.index(choice)]
-    timeNow = datetime.strptime(currentTime, "%H:%M:%S")
-    timeDif = datetime.strptime(choiceTime, "%H:%M:%S")
-    timeBegin = str(timeNow - timeDif)
-    frameData = pandas.read_csv(filePath, sep=",", header=0, usecols=[1, 2, 3, 4, 5])
-    frameColumns = ["Time", "TemperatureCurrent", "TemperatureSet", "HumidityCurrent", "HumiditySet"]
-    frameCurrent = frameData.loc[frameData["Time"] >= timeBegin, frameColumns]
-
+    try:
+        timeNow = datetime.strptime(currentTime, "%H:%M:%S")
+        timeDif = datetime.strptime(choiceTime, "%H:%M:%S")
+        timeBegin = str(timeNow - timeDif)
+        print(timeNow.time())
+        frameData = pandas.read_csv(filePath, sep=",", header=0, usecols=[1, 2, 3, 4, 5])
+        frameColumns = ["Time", "TemperatureCurrent", "TemperatureSet", "HumidityCurrent", "HumiditySet"]
+        frameCurrent = frameData.loc[frameData["Time"] >= timeBegin, frameColumns]
+        print(frameCurrent)
+    except ValueError:
+        print("Time read error")
+        filePathPrev = csvFolder + ''.join(fileList[-2])
+        frameDataPrev = pandas.read_csv(filePathPrev, sep=",", header=0, usecols=[1, 2, 3, 4, 5])
+        frameColumns = ["Time", "TemperatureCurrent", "TemperatureSet", "HumidityCurrent", "HumiditySet"]
+        frameCurrentPrev = frameDataPrev.loc[((frameDataPrev["Time"] >= "23:00:00") & (frameDataPrev["Time"] <= "23:59:59")), frameColumns]
+        frameDataNow = pandas.read_csv(filePath, sep=",", header=0, usecols=[1, 2, 3, 4, 5])
+        frameCurrentNow = frameDataPrev.loc[((frameDataNow["Time"] >= "00:00:00") & (frameDataNow["Time"] <= "02:00:00")), frameColumns]
+        frameCurrent = pandas.concat([frameCurrentPrev, frameCurrentNow], ignore_index=True)
+        print(frameCurrent)
+    except FileNotFoundError:
+        print("File not found")
+        return
+    except UnboundLocalError:
+        print("Time format error")
     figure.clear()
     lox = matplotlib.ticker.LinearLocator(18)
     graphTemp = figure.add_subplot(111)
     graphTemp.xaxis.set_major_locator(lox)
     graphTemp.set_facecolor("black")
-    graphTemp.plot(frameCurrent["Time"], frameCurrent["TemperatureCurrent"], "-b",
-                   frameCurrent["Time"], frameCurrent["TemperatureSet"], "--c")
-    graphHum = graphTemp.twinx()
-    graphHum.plot(frameCurrent["Time"], frameCurrent["HumidityCurrent"], "-r",
-                  frameCurrent["Time"], frameCurrent["HumiditySet"], "--m")
-    graphHum.set_ylabel("Влажность, %", color="red")
-    if modeIndex == 2:
-        graphTemp.fill_between(x=frameCurrent["Time"], y1=frameCurrent["TemperatureCurrent"],
+    try:
+        graphTemp.plot(frameCurrent["Time"], frameCurrent["TemperatureCurrent"], "-b",
+                            frameCurrent["Time"], frameCurrent["TemperatureSet"], "--c")
+        graphTemp.set_ylabel("Температура, °C", color="blue")
+        graphTemp.grid(alpha=0.5, linestyle="-", color="cyan", linewidth=0.3)
+        graphTemp.tick_params(labelsize=8, colors="yellow")
+        if modeIndex == 2:
+            graphTemp.fill_between(x=frameCurrent["Time"], y1=frameCurrent["TemperatureCurrent"],
                                y2=frameCurrent["TemperatureSet"], alpha=0.2)
-    if modeIndex == 3:
-        graphHum.fill_between(x=frameCurrent["Time"], y1=frameCurrent["HumidityCurrent"],
+        if modeIndex == 3:
+            graphHum = graphTemp.twinx()
+            graphHum.set_ylabel("Влажность, %", color="red")
+            graphHum.plot(frameCurrent["Time"], frameCurrent["HumidityCurrent"], "-r",
+                      frameCurrent["Time"], frameCurrent["HumiditySet"], "--m")
+            graphHum.fill_between(x=frameCurrent["Time"], y1=frameCurrent["HumidityCurrent"],
                               y2=frameCurrent["HumiditySet"], alpha=0.2)
-    graphTemp.set_ylabel("Температура, °C", color="blue")
-    graphTemp.grid(alpha=0.5, linestyle="-", color="cyan", linewidth=0.3)
-    graphHum.grid(alpha=0.6, linestyle=":", color="red")
-    graphTemp.tick_params(labelsize=8, colors="yellow")
-    graphHum.tick_params(labelsize=8, colors="yellow")
+            graphHum.grid(alpha=0.6, linestyle=":", color="red")
+            graphHum.tick_params(labelsize=8, colors="yellow")
+    except UnboundLocalError:
+        print("Plot error")
+    except TypeError:
+        print("Error read data. Need to restart")
     figure.autofmt_xdate()
     canvasGraph.draw()
     canvasGraph.get_tk_widget().place(x=10, y=250)
 
     root.after(5000, Plot)
+
+
+def SaveFigure():
+    os.mkdir(picFolder) if not os.path.isdir(picFolder) else None
+    figure.savefig(f"{picFolder}{picname}")
 
 
 root = Tk()
@@ -383,11 +425,10 @@ labelPeriods = Label(anchor="w", fg=fg, bg=bg)
 
 figure = Figure(figsize=(7.8, 4.9), dpi=100, facecolor="black")
 canvasGraph = FigureCanvasTkAgg(figure=figure, master=root)
+buttonSave = ttk.Button(command=SaveFigure)
 
 ObjectsPlace()
 GetLocalIP()
 CheckIP()
 
 root.mainloop()
-
-
